b99431772f80ddccb9afe7c88f2553f9cda1e02f
buhooolatam <131042586+buhooolatam@users.noreply.github.com>
2025-01-18 09:26:02 -0400

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDX-vvxS67Cayv6WSEb2r2QLa5SOkGStbU",
-  authDomain: "votacion-7355d.firebaseapp.com",
-  projectId: "votacion-7355d",
-  storageBucket: "votacion-7355d.appspot.com",
-  messagingSenderId: "1077960152588",
-  appId: "1:1077960152588:web:dc8d934d55748fdc7f0029",
-  measurementId: "G-ZPQ9FXFZ12",
+  apiKey: "AIzaSyDXbTcmYfqtOW8JA_ARPpAlF_cZgY_d4dY",
+  authDomain: "buhooocrm.firebaseapp.com",
+  projectId: "buhooocrm",
+  storageBucket: "buhooocrm.firebasestorage.app",
+  messagingSenderId: "27867218795",
+  appId: "1:27867218795:web:dcc9b26bb153d00abe822e",
+  measurementId: "G-NEH85NTMGJ"
 };
 
 const app = initializeApp(firebaseConfig);
